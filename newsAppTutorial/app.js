1345e1e0e38f3bf380749cb2aaf658e1f99b963e
Yassin Samuels <Y.Samuels@icloud.com>
2018-12-05 21:56:20 +0100

    trying out serviceWorker

--- newsAppTutorial/app.js
+++ newsAppTutorial/app.js
@@ -1,18 +1,42 @@
 const apiKey = '04c09c860eac4acf89db9e7290b312b4';
-const mail = document.querySelector('main');
+const main = document.querySelector('main');
+const sourceSelector = document.querySelector('#sourceSelector');
+const defaultSource = 'the-washington-post';
 
-window.addEventListener('load', e => {
+window.addEventListener('load', async e => {
     updateNews();
+    await updateSources();
+    sourceSelector.value = defaultSource;
+
+    sourceSelector.addEventListener('change', e => {
+        updateNews(e.target.value);
+    });
+
+    if('serviceWorker' in Navigator) {
+        try {
+            navigator.serviceWorker.register('/newsAppTutorial/sw.js');
+            console.log(`SW registered`);
+        } catch(error) {
+            console.log(`SW failed`);
+        }
+    }
 });
 
-async function updateNews() {
-    const res = await fetch('https://newsapi.org/v2/everything?q=apple&from=2018-12-04&to=2018-12-04&sortBy=popularity&apiKey=${apiKey}')
+async function updateSources() {
+    const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${apiKey}`);
+    const json =  await res.json();
+
+    sourceSelector.innerHTML = json.sources.map(src => `<option value="${src.id}">${src.name}</option>`).join('\n');
+}
+
+async function updateNews(source = defaultSource) {
+    const res = await fetch(`https://newsapi.org/v2/everything?q=apple&from=2018-12-04&to=2018-12-04&sortBy=popularity&apiKey=${apiKey}`);
     const json = await res.json();
 
     main.innerHTML = json.articles.map(createArticle).join('\n');
 }
 
-function createArticle(articles) {
+function createArticle(article) {
     return `
         <div class="article">
             <a href="${article.url}">
